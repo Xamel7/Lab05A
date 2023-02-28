a994df8e1f41943c649fb699e5e54971a5b91290
Joel Meadows <Joel.lewis47@yahoo.com>
2023-02-28 08:55:15 -0600

    Problem 3 done

--- js/app.js
+++ js/app.js
@@ -59,7 +59,7 @@ return [x2, y2, `${a} and ${b} and ${c} sum to ${x2}.`,`The product of ${a} and 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
